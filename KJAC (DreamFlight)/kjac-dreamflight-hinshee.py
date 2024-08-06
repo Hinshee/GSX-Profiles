9bd9a6888894760a09d0ae635fb154c961f02253
Hinshee <61280994+Hinshee@users.noreply.github.com>
2024-08-06 23:58:11 +0100

    KJAC V2
    Entire rework of the profile due to major changes to the scenery

--- KJAC (DreamFlight)/kjac-dreamflight-hinshee.py
+++ KJAC (DreamFlight)/kjac-dreamflight-hinshee.py
@@ -24,23 +24,33 @@ def TerminalNames(name, letter, priority):
 
 def StandNames(name, letter, priority):
 	return CustomizedName( "%s | Stand %s#§" % (name, letter), priority )
-  
+
 MainApronNames = TerminalNames("Commercial Apron", "", 1)
+MainApronNamesNC = TerminalNames("Commercial Apron | (NC)", "", 1)
+FBOApronNames = StandNames("FBO Apron | (NC)", "", 2)
 
 parkings = {
-    0: {
+    PARKING: {
         None: ( ),
-			1 : (MainApronNames, customOffset_Stand),
-			2 : (MainApronNames, customOffset_Stand),
-			3 : (MainApronNames, customOffset_Stand),
-			4 : (MainApronNames, customOffset_Stand),
-			5 : (MainApronNames, customOffset_Stand),
-			6 : (MainApronNames, customOffset_Stand),
-			8 : (MainApronNames, customOffset_Stand),
-			9 : (MainApronNames, customOffset_Stand),
+		1 : (MainApronNames, customOffset_Stand),
+		"1A" : (MainApronNames, customOffset_Stand),
+		"1B" : (MainApronNamesNC, customOffset_Stand),
+		2 : (MainApronNames, customOffset_Stand),
+		3 : (MainApronNames, customOffset_Stand),
+		4 : (MainApronNames, customOffset_Stand),
+		5 : (MainApronNames, customOffset_Stand),
+		6 : (MainApronNames, customOffset_Stand),
+		7 : (MainApronNames, customOffset_Stand),
+		8 : (MainApronNames, customOffset_Stand),
+		9 : (MainApronNames, customOffset_Stand),
+		10 : (FBOApronNames, customOffset_Stand),
+		11 : (FBOApronNames, customOffset_Stand),
+		12 : (FBOApronNames, customOffset_Stand),
+		13 : (FBOApronNames, customOffset_Stand),
+		14 : (FBOApronNames, customOffset_Stand),
+		15 : (FBOApronNames, customOffset_Stand),
     },
-	PARKING: {
-		None: ( ),
-			7 : (MainApronNames, customOffset_Stand),
+	0: {
+		None: ( FBOApronNames, customOffset_Stand ),
 	},
 }
